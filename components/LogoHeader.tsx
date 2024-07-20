627602d13ae3c5d702e13e7de3f478433e73d28b
MomenMustafa45 <momenkara2@gmail.com>
2024-07-20 10:41:24 +0300

    feat: logoheader component created

--- components/LogoHeader.tsx
+++ components/LogoHeader.tsx
@@ -1,11 +1,15 @@
 import { Image, View } from "react-native";
 import React from "react";
 import imgLogo from "../assets/images/splashLogo.png";
+type LogoHeaderProps = {
+  classes?: string;
+  imgStyle?: string;
+};
 
-const LogoHeader = () => {
+const LogoHeader = ({ classes, imgStyle }: LogoHeaderProps) => {
   return (
-    <View className="w-3/4 h-[100px] mx-auto">
-      <Image source={imgLogo} className="w-full h-[70px]" />
+    <View className={`w-3/4 h-[100px] mx-auto ${classes}`}>
+      <Image source={imgLogo} className={`w-full h-[70px] ${imgStyle}`} />
     </View>
   );
 };
