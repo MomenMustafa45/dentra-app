04640c0221b9442e2fd1ec90f6d465f224a765d6
MomenMustafa45 <momenkara2@gmail.com>
2024-07-24 17:47:07 +0300

    feat: chapter and welldone screens added

--- navigation/DrawerNavigation.tsx
+++ navigation/DrawerNavigation.tsx
@@ -4,11 +4,15 @@ import ProfileScreen from "@/app/ProfileScreen";
 import TopicsScreen from "@/app/TopicsScreen";
 import ScreenHeader from "@/components/ScreenHeader/ScreenHeader";
 import SelectLevelScreen from "@/app/SelectLevelScreen";
+import WelldoneScreen from "@/app/WelldoneScreen";
+import ChaptersScreen from "@/app/ChaptersScreen";
 
 export type RootDrawerParamList = {
   Profile: undefined;
   Topics: undefined;
   SelectLevel: undefined;
+  WellDone: undefined;
+  Chapters: undefined;
 };
 
 const Drawer = createDrawerNavigator<RootDrawerParamList>();
@@ -16,7 +20,7 @@ const Drawer = createDrawerNavigator<RootDrawerParamList>();
 const DrawerNavigation = () => {
   return (
     <Drawer.Navigator
-      initialRouteName="SelectLevel"
+      initialRouteName="Chapters"
       screenOptions={{
         header: () => <ScreenHeader />,
       }}
@@ -37,6 +41,8 @@ const DrawerNavigation = () => {
         component={ProfileScreen}
       />
       <Drawer.Screen name="Topics" component={TopicsScreen} />
+      <Drawer.Screen name="WellDone" component={WelldoneScreen} />
+      <Drawer.Screen name="Chapters" component={ChaptersScreen} />
     </Drawer.Navigator>
   );
 };
