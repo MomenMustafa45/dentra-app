b5e19a594ee91122cec766e912e7aeb85effdf31
MomenMustafa45 <momenkara2@gmail.com>
2024-07-27 08:55:10 +0300

    feat: profile screen added

--- app/ProfileScreen.tsx
+++ app/ProfileScreen.tsx
@@ -1,10 +1,87 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
 import React from "react";
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View className="flex-1">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View className="flex-1 py-5 flex-col  justify-center">
+            <Text
+              style={{ fontFamily: "TajwalReg" }}
+              className="text-center text-theme-primary text-lg "
+            >
+              الحد الادني للسحب هو 50 جنيه
+            </Text>
+            {/*  */}
+            <View className="my-5">
+              <Text
+                style={{ fontFamily: "TajwalReg" }}
+                className="text-center text-2xl font-bold text-theme-quinary"
+              >
+                نقاطك الحالية:
+              </Text>
+              <Text
+                style={{ fontFamily: "TajwalReg" }}
+                className="text-center text-6xl font-bold text-theme-primary my-3"
+              >
+                1000
+              </Text>
+            </View>
+            {/*  */}
+            <View className="my-5">
+              <Text
+                style={{ fontFamily: "TajwalReg" }}
+                className="text-center text-2xl font-bold text-theme-quinary"
+              >
+                يعادل بالجنيه:
+              </Text>
+              <Text
+                style={{ fontFamily: "TajwalReg" }}
+                className="text-center text-6xl font-bold text-theme-primary mt-3"
+              >
+                50
+              </Text>
+              <Text
+                style={{ fontFamily: "TajwalReg" }}
+                className="text-center -mt-2 text-theme-primary text-lg"
+              >
+                جـــنيــــه
+              </Text>
+            </View>
+            {/*  */}
+            <View>
+              <TextInput
+                placeholder="ادخل رقم المحفظة"
+                className=" bg-theme-secondry text-center w-3/4 mx-auto py-2 rounded-full text-lg"
+                keyboardType="number-pad"
+              />
+              <TouchableOpacity className="bg-theme-primary  w-1/2 my-5 mx-auto py-2 rounded-full">
+                <Text
+                  style={{ fontFamily: "TajwalReg" }}
+                  className="text-white text-lg text-center font-bold"
+                >
+                  طلب سحب
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <View className="h-20 justify-center items-center bg-slate-500">
+        <Text>Advertisement</Text>
+      </View>
     </View>
   );
 };
