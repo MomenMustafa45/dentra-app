acac48ba715abebcbc3ec6f34e7583bd3a2bc759
MomenMustafa45 <momenkara2@gmail.com>
2024-07-29 14:57:38 +0300

    style: text font family fixed

--- components/FormButton/FormButton.tsx
+++ components/FormButton/FormButton.tsx
@@ -1,18 +1,20 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 type FormButtonProps = {
   title: string;
   btnStyle?: string;
+  onPress: () => void;
 };
 
-const FormButton = ({ title, btnStyle }: FormButtonProps) => {
+const FormButton = ({ title, btnStyle, onPress }: FormButtonProps) => {
   return (
     <TouchableOpacity
+      onPress={() => onPress()}
       className={`items-center bg-theme-primary rounded-3xl p-3 my-5 ${btnStyle}`}
     >
       <Text
-        className="font-bold text-theme-quaternary text-xl"
+        className="text-theme-quaternary text-xl"
         style={{ fontFamily: "TajwalReg" }}
       >
         {title}
