800a1dbf78670c49f520836622af38d942c3312b
MomenMustafa45 <momenkara2@gmail.com>
2024-07-29 14:57:19 +0300

    feat: added react-hook-form to input component

--- components/FormInput/FormInput.tsx
+++ components/FormInput/FormInput.tsx
@@ -1,15 +1,32 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, KeyboardTypeOptions } from "react-native";
 import React from "react";
+import { Noop } from "react-hook-form";
 
 type FormInputProps = {
   placeHolder: string;
   isPassword?: boolean;
   inputStyle?: string;
+  keyboardType?: KeyboardTypeOptions;
+  onChange: (...event: any[]) => void;
+  onBlur: Noop;
+  value: string;
 };
 
-const FormInput = ({ placeHolder, isPassword, inputStyle }: FormInputProps) => {
+const FormInput = ({
+  placeHolder,
+  isPassword,
+  inputStyle,
+  keyboardType = "default",
+  onChange,
+  onBlur,
+  value,
+}: FormInputProps) => {
   return (
     <TextInput
+      onChangeText={onChange}
+      onBlur={onBlur}
+      value={value}
+      keyboardType={keyboardType}
       secureTextEntry={isPassword}
       placeholder={placeHolder}
       className={`px-4 py-4 bg-theme-secondry rounded-full text-base text-theme-quinary ${inputStyle}`}
