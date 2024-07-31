51c3c1161fa4f97d57da345a99d286111806c736
MomenMustafa45 <momenkara2@gmail.com>
2024-07-31 10:15:46 +0300

    feat: change pics of topics and titles

--- app/TopicsScreen.tsx
+++ app/TopicsScreen.tsx
@@ -1,39 +1,58 @@
 import { View, Text, Image, FlatList } from "react-native";
-import dentalanatomyImg from "../assets/images/dentalanatomy.png";
-import anatomyImg from "../assets/images/anatomy.png";
-import physoilogyImg from "../assets/images/physoilogy.png";
-import histologyImg from "../assets/images/histology.png";
-import biochemistryImg from "../assets/images/biochemistry.png";
+import DENTALBIOMATERIALS from "../assets/images/DENTALBIOMATERIALS.png";
+import GENERALPHARMACOLOGY from "../assets/images/GENERALPHARMACOLOGY.png";
+import ORALHISTOLOGY from "../assets/images/ORALHISTOLOGY.png";
+import GENERALPATHOLOGY from "../assets/images/GENERALPATHOLOGY.png";
+import ORALPATHOLOGY from "../assets/images/ORALPATHOLOGY.png";
+import GENERALMICROBIOLOGY from "../assets/images/GENERALMICROBIOLOGY.png";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { RootStackParamList } from "@/navigation/StackNavigation";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootDrawerParamList } from "@/navigation/DrawerNavigation";
 const topics = [
-  { title: "Dental Anatomy", img: dentalanatomyImg },
-  { title: "Anatomy", img: anatomyImg },
-  { title: "Physiology", img: physoilogyImg },
-  { title: "Histology", img: histologyImg },
-  { title: "Biochemistry", img: biochemistryImg },
+  { title: "DENTAL BIOMATERIALS", img: DENTALBIOMATERIALS },
+  { title: "GENERAL PHARMACOLOGY", img: GENERALPHARMACOLOGY },
+  { title: "ORAL HISTOLOGY", img: ORALHISTOLOGY },
+  { title: "GENERAL PATHOLOGY", img: GENERALPATHOLOGY },
+  { title: "ORAL PATHOLOGY", img: ORALPATHOLOGY },
+  { title: "GENERAL MICROBIOLOGY", img: GENERALMICROBIOLOGY },
 ];
 
+type TopicsScreenNavigationProp = StackNavigationProp<
+  RootDrawerParamList,
+  "Chapters"
+>;
+
 const TopicsScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<TopicsScreenNavigationProp>();
+
+  const onTopicPressHandler = () => {
+    navigation.navigate("Chapters");
+  };
 
   return (
     <View className="flex-1 px-4">
       <View className="flex-1 items-center justify-center">
         <FlatList
+          style={{ flex: 1, width: "100%" }}
           data={topics}
           renderItem={({ item }) => (
-            <View className="mx-2 flex items-center">
-              <View className="w-[150px] h-[150px] rounded-full overflow-hidden">
+            <TouchableOpacity
+              onPress={onTopicPressHandler}
+              className="mx-2 items-center min-w-[120px] max-w-[150px]"
+            >
+              <View className="w-[120px] h-[120px] rounded-full overflow-hidden">
                 <Image source={item.img} className="w-full h-full" />
               </View>
               <Text
-                style={{ fontFamily: "TajwalBold" }}
-                className="text-theme-quinary text-xl mt-2"
+                style={{ fontFamily: "TajwalBold", maxWidth: 120 }}
+                className="text-theme-quinary text-lg mt-2 text-center"
               >
                 {item.title}
               </Text>
-            </View>
+            </TouchableOpacity>
           )}
           numColumns={2}
           contentContainerStyle={{
@@ -41,7 +60,7 @@ const TopicsScreen = () => {
             rowGap: 20,
           }}
           columnWrapperStyle={{
-            justifyContent: "space-between",
+            justifyContent: "space-evenly",
           }}
         />
       </View>
