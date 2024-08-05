e1045067ad8b482953d244255cb9a767212ac5c2
MomenMustafa45 <momenkara2@gmail.com>
2024-08-05 09:51:32 +0300

    feat: stopped ss in app

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,10 @@ import StackNavigation from "@/navigation/StackNavigation";
 import FontLoader from "@/utils/FontLoader";
 import { Provider } from "react-redux";
 import { store } from "@/store/store";
+import { usePreventScreenCapture } from "expo-screen-capture";
 
 const index = () => {
+  usePreventScreenCapture();
   return (
     <Provider store={store}>
       <FontLoader>
