826091e7014c85383e0f88579640a28dd1096833
MomenMustafa45 <momenkara2@gmail.com>
2024-08-05 09:57:00 +0300

    feat: added styles and navigations to drawer

--- components/DrawerContent/DrawerContent.tsx
+++ components/DrawerContent/DrawerContent.tsx
@@ -1,28 +1,52 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { RootStackParamList } from "@/navigation/StackNavigation";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { RootNavigationParamList } from "@/navigation/StackNavigation";
 
-type SplashScreenNavigationProp = StackNavigationProp<
-  RootStackParamList,
-  "Landing"
->;
+type SplashScreenNavigationProp = DrawerNavigationProp<RootNavigationParamList>;
 
 const DrawerContent = () => {
   const navigation = useNavigation<SplashScreenNavigationProp>();
+  const [activeRoute, setActiveRoute] = React.useState<number | undefined>(0);
 
-  const signOutHandler = async () => {
-    await AsyncStorage.removeItem("userInfo");
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("state", (e) => {
+      const route = e.data.state.routes[e.data.state.index].state?.index;
+
+      setActiveRoute(route);
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  const signOutHandler = () => {
     navigation.navigate("Landing");
   };
 
   return (
-    <View>
-      <TouchableOpacity onPress={signOutHandler}>
-        <Text>Sign Out</Text>
-      </TouchableOpacity>
+    <View className="px-3 py-10 flex justify-between flex-1">
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Topics")}
+          className={`${activeRoute == 0 ? "bg-theme-tertiary" : ""} p-2 my-2`}
+        >
+          <Text style={{ fontFamily: "TajwalBold" }}>Topics</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className={`${activeRoute == 1 ? "bg-theme-tertiary" : ""} p-2 my-2`}
+          onPress={() => {
+            navigation.navigate("Profile");
+          }}
+        >
+          <Text style={{ fontFamily: "TajwalBold" }}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={signOutHandler} className="p-2 my-2">
+          <Text style={{ fontFamily: "TajwalBold" }}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
