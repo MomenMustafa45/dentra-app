f93aec04c347805698c456db220a22ba309fbe4d
MomenMustafa45 <momenkara2@gmail.com>
2024-08-13 23:30:03 +0300

    fix: flex direction fixed

--- components/ScreenHeader/ScreenHeader.tsx
+++ components/ScreenHeader/ScreenHeader.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, I18nManager } from "react-native";
 import React from "react";
 import { AntDesign, Entypo, Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native-gesture-handler";
@@ -13,12 +13,17 @@ const ScreenHeader = () => {
   const navigation = useNavigation<ScreenHeaderDrawerProp>();
 
   return (
-    <View className="bg-theme-primary flex flex-row justify-between px-4 py-4">
-      <View className="flex-row">
-        <TouchableOpacity
-          className="mr-4"
-          onPress={() => navigation.toggleDrawer()}
-        >
+    <View
+      className="bg-theme-primary flex flex-row justify-between px-4 py-4"
+      style={{
+        flexDirection: I18nManager.isRTL ? "row-reverse" : "row",
+      }}
+    >
+      <View
+        className="flex-row gap-x-2"
+        style={{ flexDirection: I18nManager.isRTL ? "row-reverse" : "row" }}
+      >
+        <TouchableOpacity onPress={() => navigation.toggleDrawer()}>
           <Entypo name="menu" size={30} color="white" />
         </TouchableOpacity>
         <TouchableOpacity
@@ -31,7 +36,10 @@ const ScreenHeader = () => {
         </TouchableOpacity>
       </View>
       {/*  */}
-      <View className="flex flex-row items-center">
+      <View
+        className="flex flex-row items-center gap-x-2"
+        style={{ flexDirection: I18nManager.isRTL ? "row-reverse" : "row" }}
+      >
         <Text
           className="text-theme-quaternary text-lg mr-2"
           style={{
@@ -40,9 +48,14 @@ const ScreenHeader = () => {
         >
           دكتور {unserInfo.name}
         </Text>
-        <View className=" overflow-hidden bg-theme-quaternary rounded-full p-1">
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Topics");
+          }}
+          className=" overflow-hidden bg-theme-quaternary rounded-full p-1"
+        >
           <Ionicons name="person" size={24} color="#C7C4BF" />
-        </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
